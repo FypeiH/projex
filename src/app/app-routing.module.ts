c2bc501df89df8755cb140b9697a67e81d6dc98a
Filipe Bravo <fypeih.developer@gmail.com>
2024-06-11 18:09:53 +0100

    :sparkles: feat: Login component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Components' imports
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserModule } from "@angular/platform-browser";
 //Components' imports
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { LoginComponent } from "./components/login/login.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
