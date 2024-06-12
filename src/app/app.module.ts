4f99d938533bb0f03e8c658bf8cff5a989ae5715
Filipe Bravo <fypeih.developer@gmail.com>
2024-06-12 19:59:40 +0100

    :sparkles: feat: Google login feature partially done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,51 @@
 // Modules' imports
 import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
 import { BrowserModule } from "@angular/platform-browser";
+import { SocialLoginModule, SocialAuthServiceConfig } from "@abacritt/angularx-social-login";
+import { ToastrModule } from "ngx-toastr";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 //Components' imports
 import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app-routing.module";
 import { LoginComponent } from "./components/login/login.component";
+
+//Providers' imports
+import {
+  GoogleLoginProvider
+} from '@abacritt/angularx-social-login';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SocialLoginModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      progressBar: true
+    })
   ],
   providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('YOUR_GOOGLE_OAUTH_CLIENT_ID', {
+              oneTapEnabled: false
+            })
+          }
+        ],
+      } as SocialAuthServiceConfig,
+    }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  user: SocialUser | null = null;
+  loggedIn: boolean = false;
+
+  constructor(private socialAuthService: SocialAuthService, private toastr: ToastrService, private router: Router) {}
+
+  ngOnInit() {
+    this.socialAuthService.authState.pipe(take(1)).subscribe((user) => {
+      this.user = user;
+      this.loggedIn = user != null;
+      
+      if (this.loggedIn) {
+        this.toastr.success('You are logged in', 'Success');
+      }
+    });
+  }
+
+  signInWithGoogle(): void {
+    google.accounts.id.prompt((notification) => {
+      if (notification.isNotDisplayed()) {
+        this.toastr.error('Google Sign-In prompt was not displayed', 'Error');
+      }
+    });
+  }
+
+  signOut(): void {
+    this.socialAuthService.signOut().then(() => {
+      this.user = null;
+      this.loggedIn = false;
+    });
+  }
 }
